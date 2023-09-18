1e20d038bd8d268c0dba4fb3062485160171039e
talhagozutok <talhagztk@gmail.com>
2023-09-18 22:15:21 +0300

    added: Elastic client

---
+++ API/Elasticsearch/Elasticsearch.API/Extensions/ElasticsearchExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch.API.Extensions;
+
+public static class ElasticsearchExtensions
+{
+    public static void AddElasticClient(
+        this IServiceCollection services,
+        IConfigurationRoot configurationRoot)
+    {
+        var elasticConfigSection = configurationRoot.GetSection("Elastic");
+        var pool = new SingleNodeConnectionPool(new Uri(elasticConfigSection["Url"]!));
+
+        var settings = new ConnectionSettings(pool);
+        var client = new ElasticClient(settings);
+
+        services.AddSingleton(typeof(ElasticClient), client);
+    }
+}
--- API/Elasticsearch/Elasticsearch.API/Program.cs
+++ API/Elasticsearch/Elasticsearch.API/Program.cs
@@ -1,9 +1,13 @@
+using Elasticsearch.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthorization();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddElasticClient(builder.Configuration);
 
 var app = builder.Build();
 
@@ -12,7 +16,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
