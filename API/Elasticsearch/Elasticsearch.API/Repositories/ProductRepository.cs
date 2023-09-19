c5d471ba76f544601f4271f1cf47c14b2bbdf256
talhagozutok <talhagztk@gmail.com>
2023-09-19 15:26:15 +0300

    added: ProductRepository

---
+++ API/Elasticsearch/Elasticsearch.API/Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using Elasticsearch.API.Models;
+using Nest;
+
+namespace Elasticsearch.API.Repositories;
+
+public class ProductRepository
+{
+    private readonly ElasticClient _elasticClient;
+
+    public ProductRepository(ElasticClient elasticClient)
+    {
+        _elasticClient = elasticClient;
+    }
+
+    public async Task<Product?> SaveAsync(Product product)
+    {
+        product.CreatedAt = DateTime.Now;
+
+        var response = await _elasticClient.IndexAsync(
+            product,
+            x => x.Index("products"));
+
+        if (!response.IsValid)
+        {
+            return null;
+        }
+
+        product.Id = response.Id;
+
+        return product;
+    }
+}
