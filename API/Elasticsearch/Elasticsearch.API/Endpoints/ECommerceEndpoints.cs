92ac546f20ee87ba28b3307cf964d171f8f5b30c
talhagozutok <talhagztk@gmail.com>
2023-09-22 12:52:22 +0300

    refactoring: Remove `Get` prefix from endpoints

--- API/Elasticsearch/Elasticsearch.API/Endpoints/ECommerceEndpoints.cs
+++ API/Elasticsearch/Elasticsearch.API/Endpoints/ECommerceEndpoints.cs
@@ -9,69 +9,69 @@ public static class ECommerceEndpoints
     {
         var routeGroup = app.MapGroup("/api/ecommerce");
 
-        routeGroup.MapGet("/termQuery", GetTermQueryAsync);
-        routeGroup.MapPost("/termsQuery", GetTermsQueryAsync);
-        routeGroup.MapGet("/prefixQuery", GetPrefixQueryAsync);
-        routeGroup.MapGet("/rangeQuery", GetRangeQueryAsync);
-        routeGroup.MapGet("/matchAll", GetMatchAllQueryAsync);
-        routeGroup.MapGet("/paginationQuery", GetPaginationQueryAsync);
-        routeGroup.MapGet("/wildcardQuery", GetWildcardQueryAsync);
-        routeGroup.MapGet("/fuzzyQuery", GetFuzzyQueryAsync);
-        routeGroup.MapGet("/matchQuery", GetMatchQueryAsync);
-        routeGroup.MapGet("/matchBoolPrefixQuery", GetMatchBoolPrefixAsync);
-        routeGroup.MapGet("/matchPhraseQuery", GetMatchPhraseQueryAsync);
+        routeGroup.MapGet("/termQuery", TermQueryAsync);
+        routeGroup.MapPost("/termsQuery", TermsQueryAsync);
+        routeGroup.MapGet("/prefixQuery", PrefixQueryAsync);
+        routeGroup.MapGet("/rangeQuery", RangeQueryAsync);
+        routeGroup.MapGet("/matchAll", MatchAllQueryAsync);
+        routeGroup.MapGet("/paginationQuery", PaginationQueryAsync);
+        routeGroup.MapGet("/wildcardQuery", WildcardQueryAsync);
+        routeGroup.MapGet("/fuzzyQuery", FuzzyQueryAsync);
+        routeGroup.MapGet("/matchQuery", MatchQueryAsync);
+        routeGroup.MapGet("/matchBoolPrefixQuery", MatchBoolPrefixAsync);
+        routeGroup.MapGet("/matchPhraseQuery", MatchPhraseQueryAsync);
     }
 
-    private static async Task<IResult> GetTermQueryAsync(
+    private static async Task<IResult> TermQueryAsync(
         string customerFirstName,
         ECommerceRepository repository)
         => Results.Ok(await repository.TermQueryAsync(customerFirstName));
 
-    private static async Task<IResult> GetTermsQueryAsync(
+    private static async Task<IResult> TermsQueryAsync(
         List<string> customerFirstNameList,
         ECommerceRepository repository)
         => Results.Ok(await repository.TermsQueryAsync(customerFirstNameList));
 
-    private static async Task<IResult> GetPrefixQueryAsync(
+    private static async Task<IResult> PrefixQueryAsync(
         string customerFirstName,
         ECommerceRepository repository)
         => Results.Ok(await repository.PrefixQueryAsync(customerFirstName));
 
-    private static async Task<IResult> GetRangeQueryAsync(
+    private static async Task<IResult> RangeQueryAsync(
         double? gte, double? lte,
         ECommerceRepository repository)
         => Results.Ok(await repository.RangeQueryAsync(gte, lte));
 
-    private static async Task<IResult> GetMatchAllQueryAsync(
+    private static async Task<IResult> MatchAllQueryAsync(
         ECommerceRepository repository)
         => Results.Ok(await repository.MatchAllQueryAsync());
 
-    private static async Task<IResult> GetPaginationQueryAsync(
+    private static async Task<IResult> PaginationQueryAsync(
         ECommerceRepository repository,
         int page = 1, int pageSize = 5)
         => Results.Ok(await repository.PaginationQueryAsync(page, pageSize));
 
-    private static async Task<IResult> GetWildcardQueryAsync(
+    private static async Task<IResult> WildcardQueryAsync(
         string customerFullNameWildcard,
         ECommerceRepository repository)
         => Results.Ok(await repository.WildcardQueryAsync(customerFullNameWildcard));
 
-    private static async Task<IResult> GetFuzzyQueryAsync(
+    private static async Task<IResult> FuzzyQueryAsync(
         string customerFirstNameWildcard,
         ECommerceRepository repository)
         => Results.Ok(await repository.FuzzyQueryAsync(customerFirstNameWildcard));
 
-    private static async Task<IResult> GetMatchQueryAsync(
+    private static async Task<IResult> MatchQueryAsync(
         string categoryName,
         ECommerceRepository repository)
         => Results.Ok(await repository.MatchQueryAsync(categoryName));
 
-    private static async Task<IResult> GetMatchBoolPrefixAsync(
+    private static async Task<IResult> MatchBoolPrefixAsync(
         string customerFullName,
         ECommerceRepository repository)
         => Results.Ok(await repository.MatchBoolPrefixAsync(customerFullName));
 
-    private static async Task<IResult> GetMatchPhraseQueryAsync(
+    private static async Task<IResult> MatchPhraseQueryAsync(
         string customerFullName,
         ECommerceRepository repository)
         => Results.Ok(await repository.MatchPhraseQueryAsync(customerFullName));
