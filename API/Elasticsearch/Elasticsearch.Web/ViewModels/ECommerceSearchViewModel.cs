c896104d85d13614784564a971b746254b5850e3
talhagozutok <talhagztk@gmail.com>
2023-09-23 10:00:55 +0300

    added: `ECommerceSearchViewModel` class

---
+++ API/Elasticsearch/Elasticsearch.Web/ViewModels/ECommerceSearchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Elasticsearch.Web.ViewModels;
+
+public class ECommerceSearchViewModel
+{
+    public string? Category { get; set; }
+    public string? Gender { get; set; }
+
+    [DisplayName("Customer Full Name")]
+    public string? CustomerFullName { get; set; }
+
+    [DisplayName("Order Date (Start)")]
+    [DataType(DataType.Date)]
+    public DateTime? OrderDateStart { get; set; }
+
+    [DisplayName("Order Date (End)")]
+    [DataType(DataType.Date)]
+    public DateTime? OrderDateEnd { get; set; }
+}
